b7064372d2470f3494c4cb06edf7409e106b12ac
Kajiku Moreau <master@zak1ck.info>
2018-04-14 01:35:19 +0900

    add

--- keyboards/lets_split/keymaps/zak1ck/keymap.c
+++ keyboards/lets_split/keymaps/zak1ck/keymap.c
@@ -37,20 +37,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * NOTE: KC_ALT=GUI, KC_GUI=Alt
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  ;:  |  '"  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  |  \|  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  Tab | GUI  |  Alt |Adjust|Lower |Space |Enter |Raise |  [{  |  ]}  |  -_  |  =+  |
+ * | Esc  | GUI  |  Alt |Adjust|Lower |Space |Enter |Raise |  [{  |  ]}  |  -_  |  =+  |
  * `-----------------------------------------------------------------------------------'
  */
 [_NORMAL] = KEYMAP( \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTRL,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
-  KC_TAB,  KC_LALT, KC_LGUI, ADJUST,  LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_LBRC, KC_RBRC, KC_MINS, KC_EQL   \
+  KC_ESC,  KC_LALT, KC_LGUI, ADJUST,  LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_LBRC, KC_RBRC, KC_MINS, KC_EQL   \
 ),
 
 /* Lower
